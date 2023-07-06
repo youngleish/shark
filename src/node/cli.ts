c906007a23c5680497daff15f5c83867e42646a1
younglei <youngleish@gmail.com>
2023-07-06 19:15:38 +0800

    feature(cli): dev server

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,14 +1,23 @@
 import {cac} from 'cac';
+import {createDevServer} from './dev';
 
 const version = require("../../package.json").version;
 
 const cli = cac("shark").version(version).help();
 
+// 调试 CLI:
+// 1. 在 package.json 中声明 bin 字段
+// 2. 通过 npm link 将命令 link 到全局
+// 3. 执行 shark dev docs 命令
+
 cli
-  .command("[root]", "start dev server")
+  .command("dev [root]", "start dev server")
   .alias("dev")
   .action(async (root: string) => {
-    console.log("dev", root); 
+    // root = root ? path.resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+    await server.listen();
+    server.printUrls();
   })
 
 cli
